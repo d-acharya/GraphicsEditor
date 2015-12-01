4c9b4c0f3e01bca7af75e1348109ff1230c0b94e
IsabelleTan <Isabelle.w.tan@gmail.com>
2015-12-01 14:46:39 +0100

    Added a task to the taskdivision and made small adjustments to MyDrawModel.java

--- src/jdraw/dacharyaitan/MyDrawModel.java
+++ src/jdraw/dacharyaitan/MyDrawModel.java
@@ -18,16 +18,17 @@ import jdraw.framework.Figure;
 import jdraw.std.EmptyDrawCommandHandler;
 
 /**
- * Provide a standard behavior for the drawing model. This class initially does not implement the methods
- * in a proper way.
- * It is part of the course assignments to do so.
+ * Provide a standard behavior for the drawing model. This class initially does
+ * not implement the methods in a proper way. It is part of the course
+ * assignments to do so.
+ * 
  * @author Dinesh Acharya and Isabelle Tan
  *
  */
 public class MyDrawModel implements DrawModel {
 	private List<Figure> figures = new ArrayList<Figure>();
 	private List<DrawModelListener> listeners = new ArrayList<DrawModelListener>();
-	
+
 	@Override
 	public void addFigure(Figure f) {
 		figures.add(f);
@@ -56,20 +57,23 @@ public class MyDrawModel implements DrawModel {
 	public void removeModelChangeListener(DrawModelListener listener) {
 		listeners.remove(listener);
 	}
-	
+
 	// Fire an event, i.e. notify all listeners
-	public void fire(DrawModelEvent e){
-		for(DrawModelListener listener : listeners){
+	public void fire(DrawModelEvent e) {
+		for (DrawModelListener listener : listeners) {
 			listener.modelChanged(e);
 		}
 	}
 
-	/** The draw command handler. Initialized here with a dummy implementation. */
+	/**
+	 * The draw command handler. Initialized here with a dummy implementation.
+	 */
 	// TODO initialize with your implementation of the undo/redo-assignment.
 	private DrawCommandHandler handler = new EmptyDrawCommandHandler();
 
 	/**
 	 * Retrieve the draw command handler in use.
+	 * 
 	 * @return the draw command handler.
 	 */
 	public DrawCommandHandler getDrawCommandHandler() {
@@ -78,16 +82,22 @@ public class MyDrawModel implements DrawModel {
 
 	@Override
 	public void setFigureIndex(Figure f, int index) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.setFigureIndex has to be implemented");
+		if (figures.contains(f)) {
+			figures.remove(f);
+			figures.add(index, f);
+			DrawModelEvent event = new DrawModelEvent(this, f, Type.DRAWING_CHANGED);
+			fire(event);
+		} else {
+			System.out.println(
+					"The figure" + f.toString() + " does not exist in the list of figures yet, and cannot be moved");
+		}
 	}
 
 	@Override
-	// Use removeFigure() instead of clear to notify the listeners for each removed figure.
 	public void removeAllFigures() {
-		for (Figure fig : figures){
-			removeFigure(fig);
-		}
+		figures.clear();
+		DrawModelEvent event = new DrawModelEvent(this, null, Type.DRAWING_CLEARED);
+		fire(event);
 	}
 
 }
